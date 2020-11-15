86e3bae7fc457527a95d8d5fe6dc71cad9b7a2c7
lucasdessy <lucasedu49@gmail.com>
2020-11-15 13:15:47 -0300

    added startupId to user

--- lib/models/usuario.dart
+++ lib/models/usuario.dart
@@ -3,20 +3,26 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Usuario {
   String id;
   String nome;
+  String idStartup;
 
 // Usuario e Senha sao gerenciados pelo firebase
 
   Usuario({
     this.id,
     this.nome,
+    this.idStartup,
   });
 
   factory Usuario.fromDocument(DocumentSnapshot snapshot) {
     return Usuario(
       id: snapshot.id,
-      nome: snapshot.data()['name'] ?? "Sem nome",
+      nome: snapshot.data()['nome'] ?? "Sem nome",
+      idStartup: snapshot.data()['id_startup'],
     );
   }
 
-  Map<String, dynamic> toJson() => {};
+  Map<String, dynamic> toJson() => {
+        "nome": nome,
+        "id_startup": idStartup,
+      };
 }
