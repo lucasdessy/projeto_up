b7ba76d4df32b6c7591287b14f103e4e4133c48a
lucasdessy <lucasedu49@gmail.com>
2020-11-15 13:40:44 -0300

    added early cards implementation for project page

--- lib/ui/pages/project_page/components/view.dart
+++ lib/ui/pages/project_page/components/view.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:projeto_up/ui/components/up_header.dart';
+import 'package:projeto_up/ui/pages/project_page/components/at_a_glance_card.dart';
 import 'package:projeto_up/ui/pages/project_page/project_page_bloc.dart';
 
 class ProjectPageView extends StatelessWidget {
   final ProjectPageController controller;
 
   const ProjectPageView({Key key, @required this.controller}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Obx(() => Text(controller.teste()));
+    return Scaffold(
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(
+          parent: AlwaysScrollableScrollPhysics(),
+        ),
+        slivers: [
+          UpHeader(),
+          ProjectPageAtAGlanceCard(),
+        ],
+      ),
+    );
   }
 }
