c8915b62897e60ff847b514b699fc7a9d9f0afbb
lucasdessy <lucasedu49@gmail.com>
2020-11-20 00:04:31 -0300

    more network assets

--- lib/utils/up_network_assets.dart
+++ lib/utils/up_network_assets.dart
@@ -2,4 +2,6 @@
 class UpNetworkAssets {
   static const String camera =
       "https://firebasestorage.googleapis.com/v0/b/projeto-up-e6f22.appspot.com/o/up-assets%2Fcamera.png?alt=media&token=caee25dd-02e2-453f-8722-dfde99ca1c47";
+  static const String noImage =
+      "https://firebasestorage.googleapis.com/v0/b/projeto-up-e6f22.appspot.com/o/up-assets%2Fx.jpg?alt=media&token=a9e35849-2f0a-45d3-a3ff-d56c53b274c0";
 }
