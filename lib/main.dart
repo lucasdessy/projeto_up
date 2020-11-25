5fed0b54c8ba131d5ea37fb98f94c7528d659569
lucasdessy <lucasedu49@gmail.com>
2020-11-25 13:28:31 -0300

    fixed device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:projeto_up/app.dart';
 import 'package:projeto_up/services/app_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await Firebase.initializeApp();
   initializeServices();
   runApp(App());
